e5969f01957917e635762baad94451d85bd19102
Rukiyath Faheema <Rukiyath.Faheema@oneadvanced.com>
2017-12-14 11:07:24 +0530

    Yaaay Java added !

---
+++ JavaText.java
@@ -0,0 +1,9 @@
+public class JavaText{
+	
+	  public static void main(String[] args) {
+		            System.out.println("\n\n\t\t****START*****");
+	       	        System.out.println("Devops Java Test ..");
+	       	        System.out.println("Checking compatibility with Jenkins ..");
+	       	        System.out.println("\t\t*****END******\n\n");  
+	  }
+}
